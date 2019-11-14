800649f672950a851989e67b36fd9eee377d5637
Tyler VanBlargan <12vanblart@gmail.com>
2019-11-14 13:41:50 -0500

    Update Powershell_Profile.ps1

--- Powershell_Profile.ps1
+++ Powershell_Profile.ps1
@@ -7,6 +7,9 @@ Set-Theme Robbyrussell
 # Change Color of Commands to Magenta (-b in git checkout -b)
 Set-PSReadlineOption -TokenKind Parameter -ForegroundColor Magenta
 
+# Change color of Operators to Red (Easier to read)
+Set-PSReadlineOption -TokenKind Operator -ForegroundColor Red
+
 # Change file colors to not be hard to read
 $global:PSColor.File.Hidden.Color = 'Green'
 
